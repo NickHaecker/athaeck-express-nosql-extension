d806df06272862f75487d5ee1779bb06541595e0
Nick Häcker <nick.philipp.haecker@hs-furtwangen.de>
2021-08-11 08:54:47 +0200

    next try

--- index.ts
+++ index.ts
@@ -3,17 +3,18 @@ import path from "path";
 import api from "./api/index";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 const enviroment = process.env.NODE_ENV;
 const isProduction = enviroment === "production";
 
 
 app.use("/api", api({}));
 
-
-
+console.log(path.join(__dirname, "../portal/dist"))
+console.log(path.join(__dirname, "../portal/dist/index.html"))
 
 if (isProduction) {
+
     app.use("/", express.static(path.join(__dirname, "../portal/dist")));
     app.get(/.*/, function (_req, res) {
         res.sendFile(path.join(__dirname, "../portal/dist/index.html"));
