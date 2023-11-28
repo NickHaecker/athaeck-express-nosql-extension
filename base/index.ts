2a67247ba5f41ec324be6f705154a04211f0a7b7
Nick Häcker <nick.athaeck@googlemail.com>
2023-11-28 11:25:32 +0100

    remove asyncs

--- base/index.ts
+++ base/index.ts
@@ -1,62 +1,62 @@
-import { BaseExpressRoute, BaseExpressRouter, ExpressRouteType } from "../athaeck-express-base/base/express";
+import {
+  BaseExpressRoute,
+  BaseExpressRouter,
+  ExpressRouteType,
+} from "../athaeck-express-base/base/express";
 import { BaseDB } from "./db/types";
-import path from 'path';
-
-
+import path from "path";
 
 export abstract class BaseNoSQLExpressRouteExtension extends BaseExpressRoute {
-    protected db: any | undefined
-    abstract dbName: string
+  protected db: any | undefined;
+  abstract dbName: string;
 
-    constructor(route: string, routeType: ExpressRouteType) {
-        super(route, routeType)
-    }
+  constructor(route: string, routeType: ExpressRouteType) {
+    super(route, routeType);
+  }
 
-    public TakeRouter(router: BaseExpressRouter): void {
-        super.TakeRouter(router)
-        const sqlRouter: BaseNoSQLExpressRouterExtension = <BaseNoSQLExpressRouterExtension>router
-        this.db = sqlRouter.Plattform?.GetDB(this.dbName)
-    }
+  public async TakeRouter(router: BaseNoSQLExpressRouterExtension): Promise<void> {
+    super.TakeRouter(router);
+
+    this.db = await router.Plattform?.GetDB(this.dbName);
+
+    console.log("0000000000", this.db);
+  }
 }
 
 export abstract class BaseNoSQLExpressRouterExtension extends BaseExpressRouter {
-    protected plattform: BaseDB | undefined
-    protected noSQLFactory: BaseNoSQLFactory
-    abstract noSQLName: string
+  protected plattform: BaseDB | undefined;
+  protected noSQLFactory: BaseNoSQLFactory;
+  abstract noSQLName: string;
 
-    constructor() {
-        super()
-        this.Init()
-    }
-    protected async Init(): Promise<void> {
-        await this.CreatePlattform()
-    }
+  constructor() {
+    super();
+    this.Init();
+    this.CreatePlattform();
 
-    public get Plattform() {
-        return this.plattform
-    }
+  }
+  protected abstract Init(): void;
+  public get Plattform() {
+    return this.plattform;
+  }
 
-    protected async CreatePlattform(): Promise<void> {
-        this.plattform = await this.noSQLFactory.CreateNoSQL(this.noSQLName)
-    }
+  protected CreatePlattform(): void {
+    this.plattform = this.noSQLFactory.CreateNoSQL(this.noSQLName);
+  }
 }
 
 export abstract class BaseNoSQLFactory {
-    protected rootFolder: string
-    protected rootDir: string
-    constructor(root: string) {
-        this.rootFolder = root
+  protected rootFolder: string;
+  protected rootDir: string;
+  constructor(root: string) {
+    this.rootFolder = root;
+  }
+  public CreateNoSQL(name: string): BaseDB | undefined {
+    const pathTo: string = path.join(this.rootDir + this.rootFolder + name);
+    const NoSQL = require(pathTo);
+    const db: BaseDB = new NoSQL();
+    if (!db) {
+      return undefined;
     }
-    public CreateNoSQL(name: string): BaseDB | undefined {
-        const pathTo: string = path.join(this.rootDir + this.rootFolder + name)
-
-        console.log(pathTo)
-        const NoSQL = require(pathTo)
-        const db: BaseDB = new NoSQL()
-        if (!db) {
-            return undefined
-        }
-        return db
-
-    }
+    return db;
+  }
 }
